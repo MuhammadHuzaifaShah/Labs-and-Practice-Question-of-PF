4d9847742b66b327fd5109baa0ab19750a57932e
Muhammad Huzaifa Shah <shahhu823@gmail.com>
2026-03-16 21:31:23 +0500

    lab 6

--- Lab-06/Task4.cpp
+++ Lab-06/Task4.cpp
@@ -1,18 +1,16 @@
 #include <iostream>
 using namespace std;
-/*Write a program that asks for a number of rows and prints a triangle where each row contains the
-Fibonacci sequence up to that point.*/
+
 int main() {
     int rows;
     cout << "Enter number of rows: ";
     cin >> rows;
-
     for (int i = 1; i <= rows; i++) {
-        int a = 0, b = 1, next;
-
-        for (int j = 1; j <= i; j++) {
+       int a = 0;
+       int b = 1;
+        for (int j = 1; j <= i; j++){
             cout << a << " ";
-            next = a + b;
+            int next = a + b;
             a = b;
             b = next;
         }
