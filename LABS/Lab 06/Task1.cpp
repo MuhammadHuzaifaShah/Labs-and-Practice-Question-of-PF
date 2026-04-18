a731c3e9ac749ff5bad860a875820e91f54cba8c
Muhammad Huzaifa Shah <shahhu823@gmail.com>
2026-04-18 23:26:41 +0500

    i modify lab 06 Task 1 for better formatting of the multiplication table. I adjusted the spacing and alignment to make it more visually appealing and easier to read. The header and the multiplication results are now properly aligned, enhancing the overall presentation of the table.

--- LABS/Lab 06/Task1.cpp
+++ LABS/Lab 06/Task1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 /* Write a program that generates a customized multiplication table (matrix) based on a range provided by
 the user. Instead of starting from 1, the table starts and ends at specific integers, creating a perfect
@@ -6,34 +7,23 @@ square grid of products.*/
 int main()
 {
     int start, end;
-
     cout << "Enter the starting number: ";
     cin >> start;
-
     cout << "Enter the ending number: ";
     cin >> end;
-
-    cout << endl;
-
     cout << "    ";
-    for (int i = start; i <= end; i++) {
-        cout << i << "   ";
+    for (int i = start; i <= end; i++)
+    {
+        cout << setw(4) << i << " ";
     }
     cout << endl;
-
-    cout << "--------------------" << endl;
-
-    for (int i = start; i <= end; i++) {
-        cout << i << " | ";
-        for (int j = start; j <= end; j++) {
-            int product = i * j;
-
-            if (product < 10)
-                cout << product << "   ";
-            else if (product < 100)
-                cout << product << "  ";
-            else
-                cout << product << " ";
+    cout << "  -----------------------" << endl;
+    for (int i = start; i <= end; i++)
+    {
+        cout << i << " |";
+        for (int j = start; j <= end; j++)
+        {
+            cout << setw(5) << i * j;
         }
         cout << endl;
     }
